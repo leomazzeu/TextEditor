87c975aa7c7782dbfd01ce095f48f9670ebae631
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-19 14:53:00 -0300

    Método de leitura de um arquivo de texto

--- Program.cs
+++ Program.cs
@@ -31,7 +31,22 @@ namespace TextEditor
     }
 
     static void Open()
-    { }
+    {
+      Console.Clear();
+      Console.WriteLine("Qual o caminho do arquivo?");
+      string path = Console.ReadLine();
+
+      using (var file = new StreamReader(path))
+      {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+      }
+
+      Console.WriteLine("");
+      Console.WriteLine("Tecle ENTER para voltar ao menu");
+      Console.ReadLine();
+      Menu();
+    }
 
     static void Update()
     {
