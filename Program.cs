2be3f1c5330a7dec2031d9c1046f523b94d5b3f9
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-19 14:24:38 -0300

    Menu criado e inicio dos métodos Open e Update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace TextEditor
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Menu();
     }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("O que você deseja fazer?");
+      Console.WriteLine("1 - Abrir novo arquivo");
+      Console.WriteLine("2 - Criar novo arquivo");
+      Console.WriteLine("0 - Sair");
+
+      short option = short.Parse(Console.ReadLine());
+
+      switch (option)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Update(); break;
+        default: Menu(); break;
+      }
+    }
+
+    static void Open()
+    { }
+
+    static void Update()
+    { }
   }
 }
